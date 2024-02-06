eab52fc85ada988e986af8cd1bcd519aab73bba8
Joaquin Uribe <joaquin.uribe.acosta@gmail.com>
2024-02-06 14:52:28 -0700

    simply reformatted to the 1920x1280 display

--- Assets/Components/Dialogue/dialogue.cs
+++ Assets/Components/Dialogue/dialogue.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 [System.Serializable]   //this makes it editable in the unity editor
 public class Dialogue
 {
+
+    //this object will simply contain information on the NPCs and what they must say, this information is editable once you instantiate it 
+    //in another object such as "dialogueTrigger"
     public string name;
 
     [TextArea(3,10)]
